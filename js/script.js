4c359930de12036d74abce9fc80ca9810465216f
Rohit Kumar <thisisrohitkumar@outlook.com>
2024-03-13 18:39:13 +0530

    update: Header UX

--- js/script.js
+++ js/script.js
@@ -10,3 +10,27 @@ closeBtn.addEventListener('click', () => {
     const mobileNav = document.querySelector('#mobile-nav')
     mobileNav.style.right = '-100%'
 })
+
+// Header visibility on scroll
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", () => {
+    let currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+        // Scroll down
+        document.getElementById("header").classList.add("header__scroll__up");
+        document.getElementById("header").classList.remove("header__scroll__down");
+        document.getElementById("mobile__header").classList.add("mobile__header__scroll__up");
+        document.getElementById("mobile__header").classList.remove("mobile__header__scroll__down");
+    } else {
+        // Scroll up
+        document.getElementById("header").classList.add("header__scroll__down");
+        document.getElementById("header").classList.remove("header__scroll__up");
+        document.getElementById("mobile__header").classList.add("mobile__header__scroll__down");
+        document.getElementById("mobile__header").classList.remove("mobile__header__scroll__up");
+    }
+    
+    lastScrollTop = currentScroll;
+});
+
